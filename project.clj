3a64c6271b11b5e736145e1d6642c172665a2c70
ryan spangler <ryan.spangler@gmail.com>
2013-06-17 10:02:36 -0700

    generating novelty

---
+++ project.clj
@@ -0,0 +1,7 @@
+(defproject honeycomb "0.0.1"
+  :description "intricate lattice of honey structure"
+  :url "http://example.com/FIXME"
+  :license {:name "Eclipse Public License"
+            :url "http://www.eclipse.org/legal/epl-v10.html"}
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [clatrix "0.3.0"]])
